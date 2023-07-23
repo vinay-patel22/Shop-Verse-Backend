3978c7b2664c660dccd40844ac5c764391bf71c5
Vinay Patel <vinaypatel5519@gmai.com>
2023-07-23 14:05:25 +0530

    Final Build

--- controller/Auth.js
+++ controller/Auth.js
@@ -79,7 +79,7 @@ exports.resetPasswordRequest = async (req, res) => {
 
     // Also set token in email
     const resetPageLink =
-      'http://ecor-email.vercel.app/reset-password?token=' + token + '&email=' + email;
+      'https://ecor-email.vercel.app/reset-password?token=' + token + '&email=' + email;
     const subject = 'reset password for e-commerce';
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
